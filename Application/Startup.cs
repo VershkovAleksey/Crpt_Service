7181b5db9468c7ae914df7d3ae0b4813b4d49086
hageb <hagebang@gmail.com>
2025-05-26 22:32:50 +0300

    fix cors

--- Application/Startup.cs
+++ Application/Startup.cs
@@ -48,10 +48,19 @@ public class Startup
         services.AddDbContext<CrptContext>(options =>
             options.UseNpgsql(Configuration.GetConnectionString("DefaultConnection")));
 
+        // services.AddCors(options =>
+        // {
+        //     options.AddPolicy("VuePolicy",
+        //         builder => builder.WithOrigins("http://130.193.52.139:8081", "http://130.193.52.139:8080", "http://130.193.52.139:80", "http://130.193.52.139")
+        //             .AllowAnyMethod()
+        //             .AllowAnyHeader()
+        //             .AllowCredentials());
+        // });
+        
         services.AddCors(options =>
         {
             options.AddPolicy("VuePolicy",
-                builder => builder.WithOrigins("http://130.193.52.139:8081", "http://130.193.52.139:8080", "http://130.193.52.139:80", "http://130.193.52.139")
+                builder => builder.AllowAnyOrigin() // Разрешаем все origins
                     .AllowAnyMethod()
                     .AllowAnyHeader()
                     .AllowCredentials());
@@ -149,9 +158,9 @@ public class Startup
             options.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
         });
         
-        app.UseRouting();
         app.UseCors("VuePolicy");
 
+        app.UseRouting();
         app.UseCurrentUserMiddleware();
         app.UseAuthentication();
         app.UseAuthorization();
